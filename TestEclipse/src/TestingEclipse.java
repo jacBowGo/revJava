6bd658c1494dbf7772af6125c12c59eb59f058b5
jacBowGo <jacbowgo@gmail.com>
2020-09-21 15:40:42 -0400

    initial commit

---
+++ TestEclipse/src/TestingEclipse.java
@@ -0,0 +1,7 @@
+
+public class TestingEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("yo");
+	}
+}
---
+++ cmd_line/HelloWorld.java
@@ -0,0 +1,7 @@
+package hellopack;
+
+public class HelloWorld {
+public static void main(String a[]) {
+System.out.println("Hello World from cmd line");
+}
+}
