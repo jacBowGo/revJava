ffa56942d27fc590f96fc2e3ed92db99ede89243
jacBowGo <jacbowgo@gmail.com>
2020-09-27 19:44:16 -0400

    added missing code

--- ASSIGNMENTS/wrappers_pract/src/eg/FloatLongDouble.java
+++ ASSIGNMENTS/wrappers_pract/src/eg/FloatLongDouble.java
@@ -4,6 +4,104 @@ public class FloatLongDouble {
 
 	public static void main(String[] args) {
 		
+		Integer i5 = 5;
+		Integer i6 = new Integer(10);
+		Float f5 = 8.23f;
+		Float f6 = new Float(7.45);
+		Long L5 = 9234L;
+		Long L6 = new Long(23420);
+		Double d5 = 2349.234;
+		Double d6 = new Double(2349.234);
+		Double d7 = 2349.234;
+		
+		System.out.println("d5 == d6: " + (d5 == d6)); 
+		System.out.println("d5 == d7: " + (d5 == d7)); //is false, should be true
+		System.out.println("d5 equals d6 : "+d5.equals(d6));
+		System.out.println("d5 equals d7 : "+d5.equals(d7));
+		
+		System.out.println("d5: " + d5);
+		System.out.println("d6: " + d6);
+		System.out.println("d7: " + d7);
+		
+		System.out.println(" ");
+		
+		Float f8 = 23.4f;
+		System.out.println("f8.compareTo(23.4) : " + f8.compareTo(23.4f));
+		System.out.println("i4.compareTo(24) : " + f8.compareTo(24f));
+		System.out.println("i4.compareTo(23) : " + f8.compareTo(23f));
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		
+		System.out.println(" ");
+		
+		Long L8 = 5267L;
+		System.out.println("i4.compareTo(5267) : " + L8.compareTo(5267L));
+		System.out.println("i4.compareTo(34567) : " + L8.compareTo(34567L));
+		System.out.println("i4.compareTo(345) : " + L8.compareTo(345L));
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		System.out.println(" ");
+		
+		Double d8 = 3672.3;
+		System.out.println("d8.compareTo(3672.3) : " + d8.compareTo(3672.3));
+		System.out.println("d8.compareTo(3673) : " + d8.compareTo(3673d));
+		System.out.println("d8.compareTo(3672) : " + d8.compareTo(3672d));
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		
+		System.out.println(" ");
+		
+		String s = "65421.32";
+		
+		double a = d8;
+		System.out.println("a: " + a);
+		
+		a = 2462;
+		d8 = a;
+		System.out.println("d8: " + d8);
+		
+		double x = Double.parseDouble(s);
+		System.out.println("x = " + x);
+		
+		System.out.println(" ");
+		
+		float b = f8;
+		System.out.println("b: " + b);
+		
+		b = 22;
+		f8 = b;
+		System.out.println("f8: " + f8);
+		
+		float y = Float.parseFloat(s);
+		System.out.println("y = " + y);
+		
+		System.out.println(" ");
+		
+		long c = L8;
+		System.out.println("c: " + c);
+		
+		a = 24235222;
+		L8 = c;
+		System.out.println("L8: " + L8);
+		
+		String s2 = "362";
+		
+		long z = Long.parseLong(s2);
+		System.out.println("z = " + z);
+		
+		System.out.println(" ");
+		
+		x = 24673.2;
+		s = x + "";
+		System.out.println("s: " + s);
+		
+		System.out.println(" ");
+		
+		
 		Integer i1 = 234;
 		Float f2 = i1.floatValue();
 		System.out.println("integer to float: " + f2);
